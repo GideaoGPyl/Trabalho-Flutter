ffcf163da4cadc780d6e97dd7df50fbf4d67d0e0
GideaoGPyl <gidepyl@gmail.com>
2026-04-08 22:49:53 -0300

    Etapa5

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -70,6 +70,11 @@ class GamePage extends StatelessWidget {
               spacing: 5.0,
               children: [for (var letter in guess) Tile(letter.char, letter.type)],
             ),
+          GuessInput(
+            onSubmitGuess: (String guess){
+              print(guess);
+            },
+          )
         ],
       ),
     );
@@ -77,3 +82,51 @@ class GamePage extends StatelessWidget {
 }
 
 //Comentário pois a etapa do devtool não tem comit
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit() {
+    onSubmitGuess(_textEditingController.text);
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              focusNode: _focusNode,
+              autofocus: true,
+              decoration: const InputDecoration(
+                border:OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController,
+              onSubmitted: (String value) {
+                _onSubmit();
+              },
+            ),
+          ),
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: const Icon(Icons.arrow_circle_up),
+          onPressed: _onSubmit,
+        ),
+      ],
+    );
+  }
+}
