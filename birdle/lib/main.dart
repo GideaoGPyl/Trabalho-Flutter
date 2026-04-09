12a807d17402d8ad906f106cdd93d1082e71341f
GideaoGPyl <gidepyl@gmail.com>
2026-04-09 19:29:34 -0300

    Etapa6

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -52,10 +52,14 @@ class Tile extends StatelessWidget {
     );
   }
 }
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   GamePage({super.key});
-  // This object is part of the game.dart file.
-  // It manages wordle logic, and is outside the scope of this tutorial.
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
   @override
@@ -63,18 +67,25 @@ class GamePage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 5.0,
         children: [
           for (var guess in _game.guesses)
             Row(
-              spacing: 5.0,
-              children: [for (var letter in guess) Tile(letter.char, letter.type)],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                    child: Tile(letter.char, letter.type),
+                  )
+              ],
             ),
           GuessInput(
             onSubmitGuess: (String guess){
-              print(guess);
+              setState(() {
+                _game.guess(guess);
+              });
             },
-          )
+          ),
         ],
       ),
     );
@@ -128,5 +139,19 @@ class GuessInput extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class Example extends StatefulWidget {
+  ExampleWidget({super.key});
+
+  @override
+  State<ExampleWidget> createState() => _ExampleWidgetState();
+}
+
+class _ExampleWidgetState extends State<ExampleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
